96b1b5f6cead250cafcab853c7fc724046beb9b7
bertille.buisson02@gmail.com <bertille.buisson02@gmail.com>
2023-10-31 13:37:23 +0000

    7

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,18 @@
+with sub as (
+  select 
+  date_date,
+  cast(purchSE_PRICE as float64) as pr_price,
+  orders_id,
+  quantity,
+  revenue,
+  pdt_id
+  from {{ source("raw", "product") }} as prod
+  full join {{ source("raw","sales") }} as sal 
+  on prod.products_id=sal.pdt_id
+)
+select
+round(sum(quantity) * sum(pr_price),2) as Purchase_cost,
+round(sum(revenue) - sum(pr_price),2) as margin
+from sub
+group by quantity,pr_price,revenue,date_date
+order by date_date

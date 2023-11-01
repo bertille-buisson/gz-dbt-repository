f3eb7e717cef52470b013517a85b1d65d0b37c0d
bertille.buisson02@gmail.com <bertille.buisson02@gmail.com>
2023-11-01 10:36:29 +0000

    9

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -5,5 +5,6 @@ select
     quantity,
     round(sum(quantity) * sum(pr_price), 2) as purchase_cost,
     round(sum(revenue) - sum(pr_price), 2) as margin
-from {{ ref("int_orders_margin") }}
-order by orders_id, date_date
+from {{ ref("int_sales_margin") }}
+group by orders_id, date_date, revenue, quantity
+order by date_date
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -12,7 +12,13 @@ with sub as (
 )
 select
 round(sum(quantity) * sum(pr_price),2) as purchase_cost,
-round(sum(revenue) - sum(pr_price),2) as margin
+round(sum(revenue) - sum(pr_price),2) as margin,
+orders_id,
+quantity,
+revenue,
+pdt_id,
+date_date,
+pr_price
 from sub
-group by quantity,pr_price,revenue,date_date
+group by quantity,pr_price,revenue,date_date,orders_id,pdt_id,pr_price
 order by date_date
